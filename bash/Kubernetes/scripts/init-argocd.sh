a3199e34ca6cbc06dbe7e34a6ecb22784ccf8cc2
SkylarHoughtonGithub <skylarhoughton1996@gmail.com>
2024-02-25 13:50:14 -0600

    added helm todos

--- bash/Kubernetes/scripts/init-argocd.sh
+++ bash/Kubernetes/scripts/init-argocd.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 # source: https://nivogt.medium.com/iac-continuous-delivery-with-crossplane-and-argocd-how-to-automate-the-creation-of-aws-eks-1523ef0e0aa#8a89
 kubectl create namespace argocd
+# helm repo add argo-cd https://argoproj.github.io/argo-helm
+# helm install argo-cd argo-cd/argo-cd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl wait --for condition=ready pod $(kubectl get pods -n argocd | awk '{if ($1 ~ "argocd-server-") print $1}') -n argocd
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
@@ -11,7 +13,7 @@ echo -e "\n"
 echo "#####"
 echo "first time admin creds"
 echo ""
-kubectl get svc -n argocd argocd-server -o jsonpath="{.status.loadBalancer.ingress[0].hostname}"
+kubectl get svc -n argocd argocd-server -o jsonpath="{.status.loadBalancer.ingress[0]}"
 echo -e "\n"
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
 echo -e "\n"
